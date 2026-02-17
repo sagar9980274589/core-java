2b4b746ad5e259d2fcad8324a8683238d2030e45
sagar9980274589 <sagarshaivaooo@gmail.com>
2026-02-17 11:19:31 +0530

    task

---
+++ Test 1..!!/WhileLoop.java
@@ -0,0 +1,13 @@
+class WhileLoop{
+	
+	
+	public static void main(String args[]){
+		int i=0;
+		while(i<11){
+			System.out.println(i);
+		}
+		
+		
+	}
+	
+}
---
+++ task 10/WhileLoop.java
@@ -0,0 +1,18 @@
+class WhileLoop{
+	
+	
+	public static void main(String args[]){
+		int i=0;
+		while(i<11){
+			System.out.println(i);
+			i++;
+		}
+		i=10;
+		do{
+			System.out.println("do while loop "+i);
+			i--;
+		}
+		while(i>=7);			
+	}
+	
+}
